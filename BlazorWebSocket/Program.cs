eb0eb30081119ffd3f01a1bdf27489410057100e
Steve Bjorg <steve.bjorg@gmail.com>
2020-08-10 19:16:30 -0700

    use App declaration

--- BlazorWebSocket/Program.cs
+++ BlazorWebSocket/Program.cs
@@ -22,6 +22,7 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using BlazorWebSocket.Common;
+using LambdaSharp.App;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -43,6 +44,9 @@ namespace BlazorWebSocket {
             // add Cognito User Pool settings by reading config file from S3 bucket
             var cognito = await http.GetFromJsonAsync<CognitoSettings>("cognito.json");
             builder.Services.AddSingleton(_ => cognito);
+
+            // initialize LambdaSharp dependencies
+            builder.AddLambdaSharp<Program>();
 
             // enable access to browser local storage
             builder.Services.AddBlazoredLocalStorage();
